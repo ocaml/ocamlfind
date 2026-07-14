eba0ac6fc51da4af5dee4e86d90a1ea0df989780
David Allsopp <david.allsopp@metastack.com>
2026-07-14 15:11:05 +0100

    Move read_ldconf to Findlib and test it in CI

--- .github/workflows/toplist.ml
+++ .github/workflows/toplist.ml
@@ -1,2 +1,8 @@
 #use "topfind";;
 #list;;
+#require "findlib";;
+let () =
+  let ld_conf = Findlib.ocaml_ldconf () in
+  let print_line = Printf.printf "  %s\n" in
+  Printf.printf "Reading %s\n" ld_conf;
+  List.iter print_line (Findlib.read_ldconf ld_conf);;
